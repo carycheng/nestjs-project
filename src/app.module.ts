2fb981928cfb4b5ca203eb56fd683cb7b55d8d32
carycheng <carycheng77@gmail.com>
2021-10-02 11:59:44 -0700

    add service configu

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,7 @@
 import { MiddlewareConsumer, Module, ValidationPipe } from '@nestjs/common';
 import { APP_PIPE } from '@nestjs/core';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config'; 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
@@ -10,12 +11,27 @@ import { Report } from './reports/report.entity';
 const cookieSession = require('cookie-session');
 
 @Module({
-  imports: [UsersModule, ReportsModule, TypeOrmModule.forRoot({
-    type: 'sqlite',
-    database: 'db.sqlite',
-    entities: [User, Report],
-    synchronize: true,
-  })],
+  imports: [UsersModule, ReportsModule, 
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: `.env.${process.env.NODE_ENV}`
+    }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService], useFactory: (config: ConfigService) => {
+        return {
+          type: 'sqlite',
+          database: config.get<string>('DB_NAME'),
+          synchronize: true,
+          entities: [User, Report]
+        }
+      }
+    }),
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: 'db.sqlite',
+      entities: [User, Report],
+      synchronize: true,
+    })],
   controllers: [AppController],
   // Global Pipe
   providers: [
