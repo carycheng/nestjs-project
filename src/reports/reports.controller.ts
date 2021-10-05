9b02ab25061aacd9519b453edcb76ef555c97ef5
carycheng <carycheng77@gmail.com>
2021-10-04 21:32:34 -0700

    added report service

--- src/reports/reports.controller.ts
+++ src/reports/reports.controller.ts
@@ -1,10 +1,16 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import { Controller, Post, Body, UseGuards } from '@nestjs/common';
+import { request } from 'express';
 import { CreateReportDto } from './dto/create-report.dto';
+import { ReportsService } from './reports.service';
+import { AuthGuard } from '../guards/auth.guard'; 
 
 @Controller('reports')
 export class ReportsController {
+    constructor(private reportsService: ReportsService) {}
+
     @Post()
+    @UseGuards(AuthGuard)
     createReport(@Body() body: CreateReportDto) {
-        
+        return this.reportsService.create(body);
     }
 }
